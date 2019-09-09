3de040dd88eff930abf16c7387c5f266e618d7a1
RockStar9 <36581591+RockStar9@users.noreply.github.com>
2019-09-09 21:31:08 +0530

    parent to child communication added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>             <div>
+                <h1>Warnings?</h1>
+            Are you sure you want to do this?
+   
+
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail author="sam" timeAgo="Today at 4:55PM"
+   content="Nice Blog post"
+   authorImage={faker.image.avatar()}/>
+            </ApprovalCard>
+ <ApprovalCard>
+ <CommentDetail  author="Alex" timeAgo="Today at 2:00PM"
+   content="I Like the subject"
+   authorImage={faker.image.avatar()}/>
+ </ApprovalCard>
+   <ApprovalCard>
+   <CommentDetail author="Jane" timeAgo="Today at 9:00PM" 
+   content="It good one"
+   authorImage={faker.image.avatar()}/>
+   
+   </ApprovalCard>
+   
+        </div>
+        
+    );
+};
+ReactDOM.render(<App />,document.querySelector('#root'));
